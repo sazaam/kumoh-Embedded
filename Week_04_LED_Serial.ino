9ff8d66f766a72b2ab375dd70a93c77427c5f6f8
sazaam <sazaam@gmail.com>
2023-10-10 11:45:49 +0900

    Update Week_04_LED_Serial.ino

--- Week_04_LED_Serial.ino
+++ Week_04_LED_Serial.ino
@@ -55,3 +55,47 @@ void loop() {
 
 
 }
+
+
+
+/*
+
+#define ledOn LOW
+#define ledOff HIGH
+
+const int ledPin =  LED_BUILTIN;
+const int buttonPin = D0;  // Wemos D1 GPIO 16
+
+int ledState = ledOff;        // the current state of the LED
+int previousState = LOW;      // the previous buttonState from the button pin
+
+unsigned long buttonTime = 0;                  // The last time the output pin was toggled
+unsigned long debounceTime = 400;              // debounceTime time (I am using a noisy button)
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState != previousState && millis() - buttonTime > debounceTime) {
+    ledState = !ledState;
+    buttonTime = millis();
+  }
+
+if (ledState == ledOn) {
+    digitalWrite(ledPin, (millis() >> 9) & 3);  //Blink
+  } else
+    digitalWrite(ledPin, ledOff);
+
+previousState = buttonState;
+}
+
+*/
+
+
+
+
+

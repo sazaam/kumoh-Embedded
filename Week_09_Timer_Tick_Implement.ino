e9eb36f64571bb4d5a8e8c6ccb7d0f8ede39b9fa
sazaam <sazaam@gmail.com>
2023-11-02 09:40:25 +0900

    Update Week_09_Timer_Tick_Implement.ino

--- Week_09_Timer_Tick_Implement.ino
+++ Week_09_Timer_Tick_Implement.ino
@@ -27,3 +27,38 @@ void loop() {
       Serial.println("PRINT 15bytes!");
   }
 }
+
+
+/* LED BLUE & RED NO SYNC EXAMPLE --> No TIMER IMPLEMENTATION */
+
+
+/*
+
+#include <mbed.h>
+#include <platform/mbed_thread.h>
+
+using namespace mbed;
+
+DigitalOut led(P0_24);
+DigitalOut led2(P0_6);
+Ticker tic;
+
+void toggle(){
+  led2 = !led2;
+}
+void setup() {
+  Serial.begin(9600);
+  tic.attach(&toggle, 1);
+}
+void loop() {
+  thread_sleep_for(1000);
+  led = !led;
+  for(int i = 0; i < 4000; i++)
+    Serial.println("PRINT 15bytes!");
+}
+
+*/
+
+
+
+

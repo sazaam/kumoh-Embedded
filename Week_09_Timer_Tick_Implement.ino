f277ea063fc6ae2884f3f446f94280330ffb2754
sazaam <sazaam@gmail.com>
2023-11-02 09:37:47 +0900

    Create Week_09_Timer_Tick_Implement.ino

---
+++ Week_09_Timer_Tick_Implement.ino
@@ -0,0 +1,29 @@
+#include <mbed.h>
+#include <platform/mbed_thread.h>
+
+using namespace mbed;
+
+#define MS 1000000
+
+DigitalOut led(P0_24);
+DigitalOut led2(P0_6);
+
+Ticker tic;
+Timer tmr;
+
+void toggle(){
+  led2 = !led2;
+}
+void setup() {
+  Serial.begin(9600);
+  tmr.start();
+  tic.attach(&toggle, 1);
+}
+void loop() {
+  if(tmr.read_us() > MS){
+    tmr.reset();
+    led = !led;
+    for(int i = 0; i < 4000; i++)
+      Serial.println("PRINT 15bytes!");
+  }
+}
